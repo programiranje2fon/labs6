b555043da857693caf3eb56238d97ab747cc5151
Nikola Milikic <nikola.milikic@gmail.com>
2018-11-09 15:17:02 +0100

    Azurirane klase prema poslednjim izmenama u tekstu.

--- src/task1/test/TestHomeAppliance.java
+++ src/task1/test/TestHomeAppliance.java
@@ -8,24 +8,19 @@ public class TestHomeAppliance {
 	public static void main(String[] args) {
 
 		HomeAppliance k1 = new HomeAppliance(true, "Laptop Acer A315");
-		HomeAppliance k2 = new HomeAppliance(true, "Canon DSLR EOS 5D");
+		HomeAppliance k2 = new HomeAppliance(true, "Laptop Acer A315");
 		
-		k1.turnOnOrOff();
 		k1.turnOnOrOff();
 		
 		System.out.println(k1.toString());
 		System.out.println(k1.equals(k2));
 		
-		
 		TV t1 = new TV(true, "Samsung UE40", 1);
-		TV t2 = new TV(true, "Samsung UE40", 1);
 		
 		t1.turnOnOrOff();
 		t1.changeProgram(7);
 		
 		System.out.println(t1.toString());
-		System.out.println(t1.toString());
-		System.out.println(t1.equals(t2));
 		
 		HomeAppliance k3 = t1;
 		
